16948814538bea5bf443b06179f876de7a5ab69f
esadeli <efrat.sadeli@gmail.com>
2018-07-10 13:07:02 +0700

    Submit Exersice2 - Fungsi Balik String;

---
+++ exersice2/balikString.js
@@ -0,0 +1,16 @@
+console.log('Exersice 2 - Balik String');
+
+
+// Difinisikan variable
+function balikString(kata){
+    var displayReverse ='';
+    for(var i = kata.length; i> 0;i--){
+        displayReverse += kata[i-1];
+    }
+    return displayReverse;
+}
+
+
+//Test Case
+console.log(balikString('Hello World'));
+console.log(balikString('This is not a game'));
